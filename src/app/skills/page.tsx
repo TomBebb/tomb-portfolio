c320c1f54732554a7fb28d1a2398aaf06e2b9562
Tom Bebbington <tombebb@protonmail.com>
2023-07-11 18:59:57 +0100

    Add basic skills page

---
+++ src/app/skills/page.tsx
@@ -0,0 +1,51 @@
+import { useMemo } from "react"
+
+type SkillType =
+    | 'lang'
+    | 'devops'
+    | 'tool'
+
+interface Skill {
+    type: SkillType
+    name: string
+    desc: string
+
+}
+const skillTypeNames: Record<SkillType, string> = {
+    lang: "Programming Languages",
+    devops: "Dev-Ops CI/CD Tools",
+    tool: "Tools"
+}
+const skillTypes: SkillType[] = Object.keys(skillTypeNames) as SkillType[];
+
+const mySkills:  Skill[] = [
+    {
+        type: "lang",
+        name: "TypeScript",
+        desc: ""
+    },
+]
+
+interface MappedSkills {
+    type: SkillType
+    skills: Skill[]
+}
+export default function () {
+    const mapped = useMemo<MappedSkills[]>(() => (skillTypes)
+        .map(v => ({ type: v, skills: mySkills.filter(s => s.type === v) })), [mySkills, skillTypes])
+    return <div>
+        {mapped.map(s => <div>
+            <div className="font-bold text-xl">
+                {skillTypeNames[s.type]}
+            </div>
+            <div>
+                {s.skills.map(s => (
+                    <div>
+                        <span className="font-bold">{s.name}</span> - {s.desc}
+                    </div>
+                ))}
+            </div>
+        </div>)}
+
+    </div>
+}
