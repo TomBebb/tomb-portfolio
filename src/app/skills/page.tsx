6471bd9f59e6438ce4f09137942a0e920b2fa7a4
Tom Bebbington <tombebb@protonmail.com>
2023-07-13 22:33:48 +0100

    Add skills

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -4,8 +4,10 @@ type SkillType =
     | 'lang'
     | 'devops'
     | 'tool'
+    | 'ui'
 
 interface Skill {
+    company: string
     type: SkillType
     name: string
     desc: string
@@ -13,16 +15,51 @@ interface Skill {
 const skillTypeNames: Record<SkillType, string> = {
     lang: "Programming Languages",
     devops: "Dev-Ops CI/CD Tools",
-    tool: "Tools"
+    tool: "Tools",
+    ui: "UI Frameworks",
 }
 const skillTypes: SkillType[] = Object.keys(skillTypeNames) as SkillType[];
 
-const mySkills:  Skill[] = [
+const mySkills: Skill[] = [
     {
+        company: "Yellowgrid",
+        type: "lang",
+        name: "JavaScript",
+        desc: "Learnt this in free time as a teen, but I improved here while working on projects involving NodeJS and web frameworks"
+    },
+    {
+        company: "Yellowgrid",
         type: "lang",
         name: "TypeScript",
-        desc: ""
+        desc: "Learnt for a better sense of safety about Javascript code I developed,.\n"+
+        "Also its very powerful typesystem helped protect against potential bugs and is often self-documenting.\nI've used this  heavily in a handful of linked-together (private) NPM React/generic libraries.",
     },
+    {
+        company: "Both",
+        type: "lang",
+        name: "C#",
+        desc: "Learnt in free-time, and covered in Level 3 Software-Development Apprenticeship. See ASP .NET Core for previous project discussion."
+    },
+    {
+        company: "Yellowgrid",
+        type: "devops",
+        name: "Docker",
+        desc: "Learnt in DevOps apprenticeship, I've compiled Docker versions of web apps and deployed them using a tool CapRover for deployment."
+    },
+    {
+        company: "Yellowgrid",
+        type: "tool",
+        name: "NodeJS",
+        desc: "I learnt this so I could use full-stack frameworks, and frontend tools that involve a build system.",
+    },
+    {
+        company: "Yellowgrid",
+        type: "ui",
+        name: "React",
+        desc: "I adapted my Typescript knowledge for the difference and learnt the different hooks.\n"+
+        "Used in a an alpha NextJS app, a NextJS app, and an internal component/utility NPM library.",
+    }
+
 ]
 
 interface MappedSkills {
@@ -32,19 +69,25 @@ interface MappedSkills {
 export default function SkillsPager() {
     const mapped = useMemo<MappedSkills[]>(() => (skillTypes)
         .map(v => ({ type: v, skills: mySkills.filter(s => s.type === v) })), [mySkills, skillTypes])
-    return <div>
+    return <dl>
         {mapped.map(s => <div key={s.type}>
-            <div className="font-bold text-xl">
+            <dt className="font-bold text-xl">
                 {skillTypeNames[s.type]}
-            </div>
-            <div>
+            </dt>
+            <dd>
                 {s.skills.map(s => (
                     <div key={s.name}>
-                        <span className="font-bold">{s.name}</span> - {s.desc}
+                        <span className="font-bold">
+                            {s.name}
+                        </span>
+                        <br />
+                        <span className="whitespace-break-spaces">
+                            {s.desc}
+                        </span>
                     </div>
                 ))}
-            </div>
+            </dd>
         </div>)}
 
-    </div>
+    </dl>
 }
